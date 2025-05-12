b4ab5328058612b256ab1c586595d9ae3cc9e57e
minnie-kang <rha0852@gmail.com>
2025-05-13 01:46:41 +0900

    :sparkles:feat: views 작성

--- accounts/views.py
+++ accounts/views.py
@@ -1,1 +1,13 @@
-from django.shortcuts import render
+from django.contrib.auth import get_user_model
+from rest_framework import generics
+from rest_framework.permissions import AllowAny
+
+from .serializers import AccountSerializer
+
+
+User = get_user_model()
+
+class AccountView(generics.GenericAPIView):
+    permission_classes = [AllowAny]
+    queryset = User.objects.all()
+    serializer_class = AccountSerializer
