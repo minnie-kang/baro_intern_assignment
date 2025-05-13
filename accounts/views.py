8f73fdcd8adf406965cbb1978230fa120cdf1f26
minnie-kang <rha0852@gmail.com>
2025-05-14 00:59:20 +0900

    :sparkles:feat: 회원가입/로그인 view 분리

--- accounts/views.py
+++ accounts/views.py
@@ -1,13 +1,25 @@
 from django.contrib.auth import get_user_model
 from rest_framework import generics
 from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
 
-from .serializers import AccountSerializer
+from .serializers import RegisterSerializer, LoginSerializer
 
 
 User = get_user_model()
 
-class AccountView(generics.GenericAPIView):
+# 회원가입 view
+class RegisterView(generics.CreateAPIView):
     permission_classes = [AllowAny]
     queryset = User.objects.all()
-    serializer_class = AccountSerializer
+    serializer_class = RegisterSerializer
+
+# 로그인 view
+class LoginView(generics.GenericAPIView):
+    permission_classes = [AllowAny]
+    serializer_class = LoginSerializer
+    
+    def post(self, request):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        return Response(serializer.validated_data)
